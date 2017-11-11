2c85c34b199b07f792e56ebeff4a9506e443c328
RyanMoven <ryanmoven@gmail.com>
2017-11-10 23:30:19 -0500

    Minute changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,8 +12,38 @@
 
 var userOptions = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"  ]
 
+var wins = 0;
+var losses = 0;
+var guesses = 0;
+var guessesLeft = 9;
+var guessesTyped =[];
+
 document.onkeyup = function() {
   var userGuess = String.fromCharCode(event.keyCode).
   toLowerCase();
   console.log(userGuess);
+
+  // Input userChoice vs computerChoice below and compare results
+
+
+
+
+
+
+
+  //html updater
+
+  var html = 
+    "<p>Guess what letter I am thinking of</p>"
+	"<p>Wins:" + wins + "</p>"
+	"<p>Losses;" + losses + "</p>"
+	"<p>Guesses Left:" + guessesLeft + "</p>"
+	"<p>Your guesses so far:" + guessesTyped + "</p>";
+
+	document.querySelectory('#game').innerHTML = html
+
+
+
+
+
 }
