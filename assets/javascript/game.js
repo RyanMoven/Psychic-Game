2d89ecea2c1f703ad8f39c8fbc77f4c6cd10b667
RyanMoven <ryanmoven@gmail.com>
2017-11-11 01:08:16 -0500

    More changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,22 +10,43 @@
  // 		Game reset when User guesses reach zero
 
 
-var userOptions = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"  ]
+var userOptions = [ "a", "b", "c", "d",];
 
 var wins = 0;
 var losses = 0;
-var guesses = 0;
 var guessesLeft = 9;
 var guessesTyped =[];
 
 document.onkeyup = function() {
-  var userGuess = String.fromCharCode(event.keyCode).
-  toLowerCase();
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
   console.log(userGuess);
+
+  var computerChoice = userOptions[Math.floor(Math.random() *userOptions.length)];
 
   // Input userChoice vs computerChoice below and compare results
 
+--guessesLeft
+guessesTyped[userGuess]
 
+if (guessesLeft === 0) {
+	alert("you lose");
+	return guessesLeft
+}
+
+if (userGuess === computerChoice){
+	++wins
+	alert("you win");
+	
+}	
+
+var html = 
+    "<p>Guess what letter I am thinking of</p>" +
+	"<p>Wins:" + wins + "</p>" +
+	"<p>Losses:" + losses + "</p>" +
+	"<p>Guesses Left:" + guessesLeft + "</p>" +
+	"<p>Your guesses so far:" + guessesTyped + "</p>";
+
+	document.querySelector('#game').innerHTML = html;
 
 
 
@@ -33,15 +54,7 @@ document.onkeyup = function() {
 
   //html updater
 
-  var html = 
-    "<p>Guess what letter I am thinking of</p>"
-	"<p>Wins:" + wins + "</p>"
-	"<p>Losses;" + losses + "</p>"
-	"<p>Guesses Left:" + guessesLeft + "</p>"
-	"<p>Your guesses so far:" + guessesTyped + "</p>";
-
-	document.querySelectory('#game').innerHTML = html
-
+ 
 
 
 
